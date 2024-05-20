b483d6acc70e26ab5d412647cb506813cafbab3b
Azhar Umarmia <m4njra@gmail.com>
2024-05-20 23:35:23 +0100

    added registration page - still need to connect valid users to their content

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,11 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users'
 urlpatterns = [
     #defualt auth URL
     path('', include('django.contrib.auth.urls')),
+    path('', views.registration, name='register')
 ]
